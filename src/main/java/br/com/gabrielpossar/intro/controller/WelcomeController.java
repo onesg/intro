853ddcf7752a5529da611fa711ba239d4a014f9f
onesg <gabriel.seno@fatec.sp.gov.br>
2022-03-16 22:18:42 -0300

    Criação da controller WelcomeController

--- src/main/java/br/com/gabrielpossar/intro/controller/WelcomeController.java
+++ src/main/java/br/com/gabrielpossar/intro/controller/WelcomeController.java
@@ -1,2 +1,33 @@
-package br.com.gabrielpossar.intro.controller;public class WelcomeController {
+package br.com.gabrielpossar.intro.controller;
+
+import br.com.gabrielpossar.intro.model.Welcome;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+/* Todas configurações de servlet */
+@RestController
+
+/* Ponto de entrada será o (/hello) */
+@RequestMapping("/hello")
+
+public class WelcomeController {
+
+    private AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/")
+    public String welcome(){
+        return "Welcome";
+    }
+
+    @RequestMapping("/{name}/{msg}")
+    public Welcome hello(@PathVariable("name") String name,
+                         @PathVariable("msg") String msg){
+
+        return new Welcome(counter.incrementAndGet(), name, msg);
+
+    }
+
 }
