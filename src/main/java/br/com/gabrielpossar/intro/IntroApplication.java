627727e95e7c251851fc6bcede3fd7430c6adefa
onesg <gabriel.seno@fatec.sp.gov.br>
2022-03-16 22:00:38 -0300

    Adicionado comentário com notações

--- src/main/java/br/com/gabrielpossar/intro/IntroApplication.java
+++ src/main/java/br/com/gabrielpossar/intro/IntroApplication.java
@@ -3,7 +3,9 @@ package br.com.gabrielpossar.intro;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// Nota: As notações possuem configurações prontas para poupar tempo e código.
 @SpringBootApplication
+
 public class IntroApplication {
 
 	public static void main(String[] args) {
