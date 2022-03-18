e4820cd38d92bfb845a999062803c44c2292c882
onesg <gabriel.seno@fatec.sp.gov.br>
2022-03-17 21:27:08 -0300

    Mudando notação do método Welcome de @PathVariable para @RequestParam

--- src/main/java/br/com/gabrielpossar/intro/controller/WelcomeController.java
+++ src/main/java/br/com/gabrielpossar/intro/controller/WelcomeController.java
@@ -3,6 +3,7 @@ package br.com.gabrielpossar.intro.controller;
 import br.com.gabrielpossar.intro.model.Welcome;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.atomic.AtomicLong;
@@ -18,8 +19,9 @@ public class WelcomeController {
     private AtomicLong counter = new AtomicLong();
 
     @RequestMapping("/")
-    public String welcome(){
-        return "Welcome";
+    public Welcome welcome(@RequestParam(value = "name", defaultValue = "FATEC Jales") String name,
+                          @RequestParam(value = "msg", defaultValue = "Seja bem vindo!") String msg){
+        return new Welcome(counter.incrementAndGet(), name, msg);
     }
 
     @RequestMapping("/{name}/{msg}")
